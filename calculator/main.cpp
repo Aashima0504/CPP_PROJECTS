0198f48f9eea56250edf1c37f36c2cbef5a22229
Aashima Makani <aashimamakani@gmail.com>
2026-06-28 16:17:59 +0530

    Initial commit: cpp_projects

---
+++ calculator/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+void miniCalculator() {
+    char op;
+    float num1, num2;
+
+    cout << "Enter operator (+, -, *, /): ";
+    cin >> op;
+
+    cout << "Enter two operands: ";
+    cin >> num1 >> num2;
+
+    switch (op) {
+        case '+':
+            cout << num1 << " + " << num2 << " = " << num1 + num2 << endl;
+            break;
+        case '-':
+            cout << num1 << " - " << num2 << " = " << num1 - num2 << endl;
+            break;
+        case '*':
+            cout << num1 << " * " << num2 << " = " << num1 * num2 << endl;
+            break;
+        case '/':
+            if (num2 != 0)
+                cout << num1 << " / " << num2 << " = " << num1 / num2 << endl;
+            else
+                cout << "Division by zero error!" << endl;
+            break;
+        default:
+            cout << "Invalid operator!" << endl;
+    }
+}
+
+int main() {
+    miniCalculator();
+    return 0;
+}
